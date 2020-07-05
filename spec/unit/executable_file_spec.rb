ff32bc54a5e5a261929361cbd8a640a54acb5ae0
Piotr Murach <pmurach@gmail.com>
2020-07-05 15:42:16 +0100

    Change to check global executable without directory parameter

--- spec/unit/executable_file_spec.rb
+++ spec/unit/executable_file_spec.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
-RSpec.describe TTY::Which, '#executable_file?' do
-  it "checks if file is executable" do
-    allow(::File).to receive(:join).with('/usr/local/bin', 'ruby').
-      and_return('/usr/local/bin/ruby')
+RSpec.describe TTY::Which, "#executable_file?" do
+  it "checks if file in directory is executable" do
+    path = "/usr/local/bin/ruby"
+    allow(::File).to receive(:join).with("/usr/local/bin", "ruby").and_return(path)
+    allow(::File).to receive(:file?).with(path).and_return(true)
+    allow(::File).to receive(:executable?).with(path).and_return(true)
 
-    allow(::File).to receive(:file?).and_return(true)
-    allow(::File).to receive(:executable?).with('/usr/local/bin/ruby').and_return(true)
+    expect(TTY::Which.executable_file?("ruby", "/usr/local/bin")).to eq(true)
+  end
 
-    expect(TTY::Which.executable_file?('ruby', '/usr/local/bin')).to eq(true)
+  it "checks if only a file is executable" do
+    allow(::File).to receive(:file?).with("ruby").and_return(true)
+    allow(::File).to receive(:executable?).with("ruby").and_return(true)
+
+    expect(TTY::Which.executable_file?("ruby")).to eql(true)
   end
 end
